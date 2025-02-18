ad0620ff240578203d97c48c746f8f8f9627b691
Krishna Khanduja <krishna.khanduja@krishna-khanduja.r8.ubvm.nutanix.com>
2025-02-18 05:38:42 +0000

    updated step5

--- aws-eks-setup/step5/foreground.sh
+++ aws-eks-setup/step5/foreground.sh
@@ -1,1 +1,1 @@
-eksctl create cluster --name test-drive-NCS-demo --region us-west-2 --without-nodegroup --vpc-private-subnets subnet-00fcf2ebd2f565302,subnet-0b1538f443c9b7b65
+eksctl create cluster --name test-drive-NCS-demo --region us-west-2 --without-nodegroup --vpc-private-subnets subnet-0a087a44e6f472714,subnet-0adf74ba4b2115826
