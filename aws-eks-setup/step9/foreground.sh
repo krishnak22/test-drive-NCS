83d461bdfcc12bb0af54a00ba7d2b7a614e2bba4
Krishna Khanduja <krishna.khanduja@krishna-khanduja.r8.ubvm.nutanix.com>
2025-02-27 06:37:59 +0000

    Updated step10

--- aws-eks-setup/step9/foreground.sh
+++ aws-eks-setup/step9/foreground.sh
@@ -1,26 +1,2 @@
-#!/bin/bash
-# create-nodegroup-yaml.sh
-# This script reads cluster details from eke-setup.env and generates a YAML file for creating a private node group.
-
-
-# Source the environment variables
-source eks_inputs.env
-
-# Name of the output YAML file
-OUTPUT_FILE="nodegroup.yaml"
-
-# Create the YAML file with the basic structure
-cat > "$OUTPUT_FILE" <<EOF
-apiVersion: eksctl.io/v1alpha5
-kind: ClusterConfig
-metadata:
-  name: ${CLUSTER_NAME}
-  region: ${REGION}
-nodeGroups:
-  - name: private-ng
-    instanceType: t3.medium
-    desiredCapacity: 1
-    privateNetworking: true
-EOF
-echo "YAML file '$OUTPUT_FILE' created successfully."
-
+eksctl create nodegroup --cluster "$CLUSTER_NAME" —-region "$REGION" --nodes-min 1 --nodes-max 1 —-node
+-type t3.medium —name test-drive-ncs-ng-1 --node-private-networking
