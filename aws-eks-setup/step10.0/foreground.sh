b916b2c56a2b7545eb27e607472bc6bc71925728
Krishna Khanduja <krishna.khanduja@krishna-khanduja.r8.ubvm.nutanix.com>
2025-02-28 06:54:58 +0000

    Updates dtep 10.0

--- aws-eks-setup/step10.0/foreground.sh
+++ aws-eks-setup/step10.0/foreground.sh
@@ -4,12 +4,22 @@
 ENV_FILE="eks_inputs.env"
 
 # Keep asking until a valid primary_owner is entered
-while [[ -z "$PRIMARY_OWNER" ]]; do
+while true; do
+    # Ask the user for the primary_owner value
     read -p "Enter the primary_owner value: " PRIMARY_OWNER
+
+    # Check if the input is empty
+    if [[ -z "$PRIMARY_OWNER" ]]; then
+        echo "Error: primary_owner cannot be empty. Please enter a valid value."
+    else
+        # If the input is not empty, break the loop
+        break
+    fi
 done
 
-# Append primary_owner to the env file
+# Append the primary_owner value to the env file
 echo "PRIMARY_OWNER=$PRIMARY_OWNER" >> "$ENV_FILE"
 
+# Inform the user that the value has been set
 echo "primary_owner has been set to: $PRIMARY_OWNER"
 
