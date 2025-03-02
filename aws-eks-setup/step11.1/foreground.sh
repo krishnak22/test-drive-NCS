ce4eafc9a08ceec528c438dde90a10bc7003a366
Krishna Khanduja <krishna.khanduja@krishna-khanduja.r8.ubvm.nutanix.com>
2025-03-02 07:23:08 +0000

    Updated json for testing

--- aws-eks-setup/step11.1/foreground.sh
+++ aws-eks-setup/step11.1/foreground.sh
@@ -4,7 +4,7 @@
 source eks_inputs.env
 
 # Create the IAM service account
-eksctl create iamserviceaccount --cluster "$CLUSTER_NAME" --region "$REGION" --name ncs-infra-sa-new-2 --namespace ncs-infra-deployment-operator-system --approve
+eksctl create iamserviceaccount --cluster "$CLUSTER_NAME" --region "$REGION" --name ncs-infra-sa-new-2 --namespace ncs-infra-deployment-operator-system --attach-policy-arn arn:aws:iam::353502843997:policy/test-drive-ncs-bf-op-policy-1 --approve
 
 # Add service account name and namespace to eks_inputs.env
 echo "SERVICE_ACCOUNT_NAME=ncs-infra-sa-new-2" >> eks_inputs.env
