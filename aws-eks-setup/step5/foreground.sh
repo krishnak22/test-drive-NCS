1d61cf34ffeaf5663ad1946d570bcc7fe52e3a53
Krishna Khanduja <krishna.khanduja@krishna-khanduja.r8.ubvm.nutanix.com>
2025-03-04 07:01:11 +0000

    Updated dtep5

--- aws-eks-setup/step5/foreground.sh
+++ aws-eks-setup/step5/foreground.sh
@@ -1,3 +1,5 @@
+ENV_FILE="eks_inputs.env"
+
 # List of valid AWS regions
 VALID_REGIONS=("us-east-1" "us-east-2" "us-west-1" "us-west-2" "eu-central-1" "eu-west-1" "eu-west-2" "ap-south-1" "ap-northeast-1")
 
