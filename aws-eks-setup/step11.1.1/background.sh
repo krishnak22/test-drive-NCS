7092369a40ad7de8d5b5d1b034cba0378e4fa997
Krishna Khanduja <krishna.khanduja@krishna-khanduja.r8.ubvm.nutanix.com>
2025-03-06 13:14:01 +0000

    Updated step11.1.1

--- aws-eks-setup/step11.1.1/background.sh
+++ aws-eks-setup/step11.1.1/background.sh
@@ -7,6 +7,8 @@ source eks_inputs.env
 echo "SERVICE_ACCOUNT_NAME=ncs-infra-sa-new-2" >>/root/eks_inputs.env
 echo "SERVICE_ACCOUNT_NAMESPACE=ncs-infra-deployment-operator-system" >> /root/eks_inputs.env
 
+
+source eks_inputs.env
 # Retrieve the role name and ARN of the service account
 SA_ROLE_NAME=$(kubectl get sa "$SERVICE_ACCOUNT_NAME" -n "$SERVICE_ACCOUNT_NAMESPACE" -o jsonpath='{.metadata.annotations.eks\.amazonaws\.com/role-arn}' | awk -F'/' '{print $NF}')
 echo "SA_ROLE_NAME=$SA_ROLE_NAME" >> /root/eks_inputs.env
