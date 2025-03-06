ddf991362c341a9e6d8339cd58d1c1505efae8ed
Krishna Khanduja <krishna.khanduja@krishna-khanduja.r8.ubvm.nutanix.com>
2025-03-06 10:23:03 +0000

    UPDATED SCENARIO WITH CSI STEPS

--- aws-eks-setup/step15/background_2.sh
+++ aws-eks-setup/step15/background_2.sh
@@ -4,13 +4,18 @@ PYTHON_FILE="/root/py_script_1.py"
 
 cat << EOF >> "/root/py_script_1.py"
 from ruamel.yaml import YAML
+  
+yaml = YAML()
 
 with open("/root/nutanix-csi-storage/values.yaml", "r") as f:
     data = yaml.load(f)
 
 data['ntnxInitConfigMap']['usePC'] = False
+data['createPrismCentralSecret'] = False
+data['createSecret']= False
 
-with open("file.yaml", "w") as f:
+with open("/root/nutanix-csi-storage/values.yaml", "w") as f:
     yaml.dump(data, f)
-
 EOF
+
+
--- aws-eks-setup/step15/background_3.sh
+++ aws-eks-setup/step15/background_3.sh
@@ -2,5 +2,5 @@ VIP=$(kubectl get ncscluster testdrive-ncs -n ncs-system -o jsonpath='{.metadata
 
 PASSWORD=$(kubectl get -n ncs-system secrets/testdrive-ncs-init-creds -ogo-template='{{index .data "cvm-creds" | base64decode}}' | base64 -d)
 
-echo "PASSWORD=$PASSWORD" >> "/root/eks_inputs.env"
-echo "VIP=$VIP" >> "root/eks_inputs.env"
+echo "PASSWORD=$PASSWORD" >> /root/eks_inputs.env
+echo "VIP=$VIP" >> /root/eks_inputs.env
