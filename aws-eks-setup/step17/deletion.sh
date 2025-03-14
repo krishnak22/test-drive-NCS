f418c2e801a44919b2f72168ba98dd131f8f0303
Krishna Khanduja <krishna.khanduja@krishna-khanduja.r8.ubvm.nutanix.com>
2025-03-14 05:45:34 +0000

    Updated index.json tille service account creation for video recording

--- aws-eks-setup/step17/deletion.sh
+++ aws-eks-setup/step17/deletion.sh
@@ -2,8 +2,10 @@ helm delete nutanix-csi -n ntnx-system
 
 kubectl delete ncscluster testdrive-ncs -n ncs-system
 
+source eks_inputs.env
 kubectl delete ncsinfra $NCS_INFRA_NAME -n $SERVICE_ACCOUNT_NAMESPACE
 
+source eks_inputs.env
 kubectl delete workernode $WORKERNODE_NAME -n $SERVICE_ACCOUNT_NAMESPACE
 
 kubectl delete CustomResourceDefinition ncsinfras.ncs.nutanix.com
@@ -15,4 +17,4 @@ source eks_inputs.env
 eksctl delete nodegroup --cluster $CLUSTER_NAME --name td-ncs-ng-1 --region $REGION --drain=false
 
 source eks_inputs.env
-controlplane $ eksctl delete cluster --name $CLUSTER_NAME --region $REGION
+eksctl delete cluster --name $CLUSTER_NAME --region $REGION
