2a89534d45216abbaa086739221289fb4d5d69ff
Krishna Khanduja <krishna.khanduja@krishna-khanduja.r8.ubvm.nutanix.com>
2025-03-14 06:21:13 +0000

    Updated calico step

--- aws-eks-setup/step7/calico_installation.sh
+++ aws-eks-setup/step7/calico_installation.sh
@@ -2,13 +2,10 @@ source eks_inputs.env
 
 echo -e "\nInstalling Calico on EKS cluster '$CLUSTER_NAME'..."
 
-# Step 3.1: Remove AWS VPC CNI Daemonset
 kubectl delete daemonset -n kube-system aws-node
 
-# Step 3.2: Install the Tigera Operator
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.29.2/manifests/tigera-operator.yaml
 
-# Step 3.3: Configure Calico installation
 kubectl create -f - <<EOF
 kind: Installation
 apiVersion: operator.tigera.io/v1
