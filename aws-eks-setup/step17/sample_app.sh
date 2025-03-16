fda94058dc7935ab767dc4575e92bfd11f9a2940
Krishna Khanduja <krishna.khanduja@krishna-khanduja.r8.ubvm.nutanix.com>
2025-03-16 12:03:43 +0000

    Updated app deployment part

---
+++ aws-eks-setup/step17/sample_app.sh
@@ -0,0 +1,18 @@
+cat << EOF >> /root/sample_app.md
+kubectl exec -it -n postgres-db postgres bash
+psql -U admin -d postgresdb
+CREATE TABLE cars (
+  brand VARCHAR(255),
+  model VARCHAR(255),
+  year INT
+);
+SELECT * FROM cars;
+INSERT INTO cars (brand, model, year)
+VALUES ('Ford', 'Mustang', 1964);
+INSERT INTO cars (brand, model, year)
+VALUES
+  ('Volvo', 'p1800', 1968),
+  ('BMW', 'M1', 1978),
+  ('Toyota', 'Celica', 1975);
+SELECT * FROM cars;
+EOF
