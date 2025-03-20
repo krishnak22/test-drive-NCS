ef669c1077bf897d83d70dde91952bbccf6f1a89
Krishna Khanduja <krishna.khanduja@krishna-khanduja.r8.ubvm.nutanix.com>
2025-03-20 08:37:18 +0000

    Updated index.json

--- aws-eks-setup/step11/operator_inputs.sh
+++ aws-eks-setup/step11/operator_inputs.sh
@@ -1,6 +1,6 @@
 source eks_inputs.env
 
-echo "WORKER_NODE_NAME=$CLUSTER_NAME-cn-aos-wn" >> eks_inputs.env
-echo "NODE_POOL_NAME=$CLUSTER_NAME-cn-aos-np" >> eks_inputs.env
+echo "WORKER_NODE_NAME=$CLUSTER_NAME-wn" >> eks_inputs.env
+echo "NODE_POOL_NAME=$CLUSTER_NAME-np" >> eks_inputs.env
 echo "NCS_CLUSTER_NAME=$CLUSTER_NAME-cn-aos-cl" >> eks_inputs.env
 echo "NCS_INFRA_NAME=$CLUSTER_NAME-cn-aos-infra" >> eks_inputs.env
