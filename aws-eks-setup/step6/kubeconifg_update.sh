86a7131fb2b67d7eeedfffec455f6d0a496c6b74
Krishna Khanduja <krishna.khanduja@krishna-khanduja.r8.ubvm.nutanix.com>
2025-04-04 05:42:39 +0000

    Updated input steps

--- aws-eks-setup/step6/kubeconifg_update.sh
+++ aws-eks-setup/step6/kubeconifg_update.sh
@@ -1,5 +1,5 @@
 source eks_inputs.env
 
 aws eks update-kubeconfig --name "$CLUSTER_NAME" --region "$REGION"
-
+echo "CLUSTER_ARN=$(aws eks describe-cluster --name $CLUSTER_NAME --region $REGION --query "cluster.arn" --output text)" >> eks_inputs.env
 eksctl delete addon --cluster "$CLUSTER_NAME" --region $REGION --name metrics-server
