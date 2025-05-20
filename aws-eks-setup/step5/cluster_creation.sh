862cfa095b19e4e92254ab5af6bc67fd16c2dacf
Krishna Khanduja <krishna.khanduja@krishna-khanduja.r8.ubvm.nutanix.com>
2025-05-20 05:39:21 +0000

    Added tags for cluster creation step

--- aws-eks-setup/step5/cluster_creation.sh
+++ aws-eks-setup/step5/cluster_creation.sh
@@ -1,10 +1,9 @@
 if [[ ! -f eks_inputs.env ]]; then
     echo "Error: eks_inputs.env file not found. Run './eks_input.sh' first."
-fi
-
+else
 source eks_inputs.env
 
 echo -e "\nStarting EKS cluster creation..."
-eksctl create cluster --name "$CLUSTER_NAME" --region "$REGION" --version 1.30 --vpc-private-subnets "$VPC_SUBNETS" --without-nodegroup --tags primary-owner="$PRIMARY_OWNER"
-
+eksctl create cluster --name "$CLUSTER_NAME" --region "$REGION" --version 1.30 --vpc-private-subnets "$VPC_SUBNETS" --without-nodegroup --tags primary-owner="$PRIMARY_OWNER", platform="KILLERCODA", creation_time=$(date -u +%Y-%m-%dT%H:%M:%SZ)
+fi
 
