f0ccc52e8e888e82769f52c39d4b808ea5253c2d
Krishna Khanduja <krishna.khanduja@krishna-khanduja.r8.ubvm.nutanix.com>
2025-05-20 06:28:02 +0000

    Updated nodegroup_creation.sh tags

--- aws-eks-setup/step8/nodegroup_creation.sh
+++ aws-eks-setup/step8/nodegroup_creation.sh
@@ -1,4 +1,4 @@
 CREATION_TIME=$(date -u +%Y-%m-%dT%H:%M:%SZ)
 source eks_inputs.env
 
-eksctl create nodegroup --cluster "$CLUSTER_NAME" --region "$REGION" --nodes-min 1 --nodes-max 1 --node-type t3.medium --name td-ncs-ng-1 --node-private-networking --tags kc-cluster-name="$CLUSTER_NAME",creation-time="$CREATION-TIME"
+eksctl create nodegroup --cluster "$CLUSTER_NAME" --region "$REGION" --nodes-min 1 --nodes-max 1 --node-type t3.medium --name td-ncs-ng-1 --node-private-networking --tags kc-cluster-name="$CLUSTER_NAME",creation-time="$CREATION_TIME"
