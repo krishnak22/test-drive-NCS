5f73d0aa8e5a19c1fb1aa8f53dec6344d77ee12d
Krishna Khanduja <krishna.khanduja@krishna-khanduja.r8.ubvm.nutanix.com>
2025-05-20 07:02:32 +0000

    Cchecking resources created by bf through tags

--- aws-eks-setup/step12.1/yaml_creation.sh
+++ aws-eks-setup/step12.1/yaml_creation.sh
@@ -707,6 +707,7 @@ spec:
     aws:
       tags:
         primary_owner: $PRIMARY_OWNER
+	platform: KILLERCODA
       instanceType: $INSTANCE_TYPE
       amiType: AL2_x86_64
       amiReleaseVersion: 1.30.7-20250103
@@ -744,6 +745,7 @@ spec:
     aws:
       tags:
         primary_owner: $PRIMARY_OWNER
+	platform: KILLERCODA
   version: 1.0.0-5831 
 EOF
 
