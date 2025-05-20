9f7e5a1370625be8c62c7645292bd058d7285b03
Krishna Khanduja <krishna.khanduja@krishna-khanduja.r8.ubvm.nutanix.com>
2025-05-20 06:19:03 +0000

    Updated cluster_creation.sh for tags

--- aws-eks-setup/step5/cluster_creation.sh
+++ aws-eks-setup/step5/cluster_creation.sh
@@ -1,9 +1,10 @@
 if [[ ! -f eks_inputs.env ]]; then
     echo "Error: eks_inputs.env file not found. Run './eks_input.sh' first."
 else
+CREATION_TIME=$(date -u +%Y-%m-%dT%H:%M:%SZ)
 source eks_inputs.env
 
 echo -e "\nStarting EKS cluster creation..."
-eksctl create cluster --name "$CLUSTER_NAME" --region "$REGION" --version 1.30 --vpc-private-subnets "$VPC_SUBNETS" --without-nodegroup --tags primary-owner="$PRIMARY_OWNER", platform="KILLERCODA", creation_time=$(date -u +%Y-%m-%dT%H:%M:%SZ)
+eksctl create cluster --name "$CLUSTER_NAME" --region "$REGION" --version 1.30 --vpc-private-subnets "$VPC_SUBNETS" --without-nodegroup --tags primary-owner="$PRIMARY_OWNER",platform="KILLERCODA",creation-time="$CREATION_TIME"
 fi
 
